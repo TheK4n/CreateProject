4d99e6a6c599d1c464ffc6de88a9bed209f1e6a5
pendos <djvlad967891@gmail.com>
2021-08-17 23:33:44 +0300

    upd func

--- test/tests.py
+++ test/tests.py
@@ -4,6 +4,7 @@ import unittest
 def get_script_name(project_name: str) -> str:
     g = (i for i in project_name)
     shift = 0
+    flag = False
     for i in range(len(project_name)):
         char = next(g)
 
@@ -11,24 +12,22 @@ def get_script_name(project_name: str) -> str:
             start_ = project_name[:i + shift]
             end_ = project_name[i + shift:]
 
+            try:
+                if end_[1].isupper():
+                    if not flag:
+                        flag = True
+                    else:
+                        continue
+            except IndexError:
+                continue
             project_name = start_ + '-' + end_
             shift += 1
 
     return project_name.lower()
 
 
-def is_project_name_camelcase(project_name: str) -> bool:
-
-    if project_name[0].islower():
-        return False
-
-    if project_name.islower():
-        return False
-
-    for i in ['-', '_']:
-        if i in project_name:
-            return False
-    return True
+def is_camel_case(string):
+    return string != string.lower() and string != string.upper() and "_" not in string and "-" not in string
 
 
 class MyTestCase(unittest.TestCase):
@@ -37,8 +36,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(get_script_name('Imagecrypt'), 'imagecrypt')
         self.assertEqual(get_script_name('ImageCrypt'), 'image-crypt')
         self.assertEqual(get_script_name('imagecrypt'), 'imagecrypt')
-        self.assertEqual(get_script_name('imagecryptT'), 'imagecrypt-t')
-        self.assertEqual(get_script_name('ImageCryptTEST'), 'image-crypt-t-e-s-t')
+        self.assertEqual(get_script_name('imagecryptT'), 'imagecryptt')
+        self.assertEqual(get_script_name('ImageCryptTEST'), 'image-crypt-test')  #
+        self.assertEqual(get_script_name('ImageCRYPTTest'), 'image-crypt-test')  #
 
         self.assertEqual(get_script_name('ImageCryptSave'), 'image-crypt-save')
         self.assertEqual(get_script_name('ImageCryptSave'), 'image-crypt-save')
@@ -48,13 +48,16 @@ class MyTestCase(unittest.TestCase):
                          'image-crypt-save-sega-mega-drive-ultra-super')
 
     def test_is_project_name_camelcase(self):
-        self.assertEqual(is_project_name_camelcase('ImageCrypt'), True)
-        self.assertEqual(is_project_name_camelcase('Imagecrypt'), True)
+        self.assertTrue(is_camel_case('camelCase'))
+        self.assertTrue(is_camel_case('CamelCase'))
+        self.assertTrue(is_camel_case('Camelcase'))
+        self.assertTrue(is_camel_case('Case'))
 
-        self.assertEqual(is_project_name_camelcase('image-crypt'), False)
-        self.assertEqual(is_project_name_camelcase('image_crypt'), False)
-        self.assertEqual(is_project_name_camelcase('image_Crypt'), False)
-        self.assertEqual(is_project_name_camelcase('imageCrypt'), False)
+        self.assertFalse(is_camel_case('camel'))
+        self.assertFalse(is_camel_case('CAMELCASE'))
+        self.assertFalse(is_camel_case('camelcase'))
+        self.assertFalse(is_camel_case('camel_case'))
+        self.assertFalse(is_camel_case('camel-Case'))
 
 
 if __name__ == '__main__':
