96aed209a42a349aa91bead64f9c65e04078d07c
Pendosv <djvlad967891@gmail.com>
2021-08-22 00:40:40 +0300

    add -f and --force argument

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
+from argparse import Namespace
 from os import mkdir, path, system, chdir, name
 from sys import stderr
 from pathlib import Path
 
 __all__ = ['get_script_name', 'is_camel_case', 'make_dirs', 'write_files',
            'git_init', 'create_symbolic_link', 'secret', 'error_pars',
-           'init_venv', 'init_all']
+           'init_venv', 'init_all', 'create_project_path']
 
 
 def get_script_name(project_name: str, splitter: str = '-') -> str:
@@ -47,18 +48,56 @@ def is_camel_case(string: str) -> bool:
     return string != string.lower() and string != string.upper() and not bid
 
 
+def be_quiet(input_: str, is_quiet: bool = False) -> str:
+    if not is_quiet:
+        return input_
+
+    if name == 'posix':
+        return input_ + ' 1>/dev/null'
+    return input_
+
+
 def error_pars(script_name: str, msg: str, exit_code: int = 1):
     print(f'{script_name}: error: {msg}', file=stderr)
     if exit_code != 0:
-        exit(exit_code)
-    return
+        exit(1)
+    return exit_code
 
 
-def to_null(is_quiet: bool) -> str:
-    if name == 'posix':
-        return '1>/dev/null' if is_quiet else ''
-    else:
-        return ''
+def try_mkdir(project_path: str, prog_name: str, is_quiet: bool = False) -> int:
+    try:
+        mkdir(project_path)
+        if not is_quiet:
+            print(f'mkdir: created directory "{project_path}"')
+
+        return 0
+    except PermissionError:
+        error_pars(prog_name, f'permission denied: "{project_path}"')
+        return 1
+
+
+def create_project_path(prog_name: str, project_path: str, args: Namespace, is_quiet: bool = False) -> int:
+
+    try:
+        return try_mkdir(project_path, prog_name, is_quiet)
+    except FileExistsError:
+        if args.no_ask_force:
+            system(f'rm -rf {project_path}')
+            if not is_quiet:
+                print(f'project "{project_path}" removed')
+            return try_mkdir(project_path, prog_name, is_quiet)
+        elif args.ask_force:
+            ans = input(f'{prog_name}: re-write existing project "{project_path}"? [Y/n] ')
+            if ans in {'Y', 'y'}:
+                system(f'rm -rf {project_path}')
+                if not is_quiet:
+                    print(f'project "{project_path}" removed')
+                return try_mkdir(project_path, prog_name, is_quiet)
+            else:
+                raise StopIteration
+        else:
+            error_pars(prog_name, f'project "{project_path}" already exists, use -f', exit_code=0)
+            raise StopIteration
 
 
 def make_dirs(project_path, dirs: list[str]):
@@ -74,17 +113,16 @@ def write_files(project_path, files):
 
 def init_venv(project_path: str, is_quiet: bool) -> str:
     venv_path = path.join(project_path, "venv")
-    system(f'virtualenv {venv_path} {to_null(is_quiet)}')  # виртуальное окружение
+    system(be_quiet(f'virtualenv {venv_path}', is_quiet))  # виртуальное окружение
     return venv_path
 
 
 def git_init(project_path, is_quiet: bool = False) -> list[int]:
-    text_to_null = to_null(is_quiet)
     status = []
     chdir(project_path)
-    status.append(system(f'git init {text_to_null}'))
-    status.append(system(f'git add . {text_to_null}'))
-    status.append(system(f'git commit -m "Initial commit" {text_to_null}'))
+    status.append(system(be_quiet(f'git init', is_quiet)))
+    status.append(system(be_quiet(f'git add .', is_quiet)))
+    status.append(system(be_quiet(f'git commit -m "Initial commit"', is_quiet)))
     chdir('..')
     return status
 
